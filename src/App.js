c295089d1c504f5e969eefc38ee992c67472ba12
somaia-khalil <sumaya_khair@live.com>
2020-04-02 11:08:54 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+
+import React from 'react'
+
+export default class App extends React.Component{
+
+    constructor(){
+        super()
+        this.state ={
+            people: []
+        }
+    }
+
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then (people => this.setState({
+            people: people.people
+        }))
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.people.map}
+            </div>
+        )
+    }
+}
